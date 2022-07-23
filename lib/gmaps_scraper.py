901ce981401f9118bd9826c00bf7edc7bd5dbae2
Francesco Ramoni <francesco.ramoni@finleap.com>
2022-07-24 00:24:52 +0200

    Add newline

--- lib/gmaps_scraper.py
+++ lib/gmaps_scraper.py
@@ -1,5 +1,4 @@
 import logging
-import re
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
